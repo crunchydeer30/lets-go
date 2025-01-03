4b81fe96cc2392997622decf611acd6045f1fa8f
crunchydeer30 <crunchydeer30@gmail.com>
2025-01-03 12:04:54 +0300

    ch4

--- pt2/internal/data/runtime.go
+++ pt2/internal/data/runtime.go
@@ -1,6 +1,13 @@
 package data
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -8,4 +15,25 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := fmt.Sprintf("%q", jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(data []byte) error {
+	unqoutedJSONValue, err := strconv.Unquote(string(data))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unqoutedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+	return nil
 }
